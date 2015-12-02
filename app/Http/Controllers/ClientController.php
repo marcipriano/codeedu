12363a89a5dccf58503d20631a71494ae0141ca9
Marcilio Cipriano <marciliocipriano@hotmail.com>
2015-12-02 18:49:35 -0300

    ajustes no ClientController

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -53,9 +53,24 @@ class ClientController extends Controller
      */
     public function update(Request $request, $id)
     {
-        
+        /*
+        nao achei aonde poderia mandar uma msg pra vcs. gostaria de saber se tem 
+        como agilizar o processo de liberaçao dos modulos. ja tenho conhecimento 
+        de laravel 5.1(fiz o curso do vedoveli) e angular(estudei so). Não disponho de 
+        tempo(to virando a noite) e preciso da liberação rapida pois irei usar em uma 
+        nova aplicação que. 
+        meu interesse em contratar era mais saber como integrar os 2 de forma correta 
+        e tbm pela parte da seguraça da api rest desenvolvida. tenho que evitar que a 
+        api seja consumida diretamente - sem ser por request interna do sistema.
+        */
+
         $client = $this->show($id);
         $client->fill($request->all())->save();
+
+        /*
+        tentei fazer de varias formas e sempre tava dando o erro citado.
+        dessa forma foi sem erro. se tiver alguma melhor de ser aplicada me informe
+        */
         return [$client];
     }
 
