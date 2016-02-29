ec225e998c83f00e88afb3120b88dd3c06231c33
Marcilio Cipriano <marciliocipriano@hotmail.com>
2016-02-29 18:19:57 -0300

    projeto fase 2

---
+++ resources/assets/js/controllers/login.js
@@ -0,0 +1,25 @@
+angular.module('app.controllers')
+.controller('LoginController', ['$scope', '$location', 'OAuth', function($scope, $location, OAuth){
+	$scope.user = {
+		username: '',
+		password: ''
+	};
+	$scope.error = {
+		message: '',
+		error: false
+	};
+
+	$scope.login = function(){
+		if ($scope.formLogin.$valid) {
+			OAuth.getAccessToken($scope.user).then(function(){
+				$location.path('home');
+			}, function(data){
+				$scope.error.error = true;
+				$scope.error.message = data.data.error_description;
+			});
+		}else{
+			alert('Preencha todos os campos do formulario!');
+		};
+	}
+
+}]);
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" ng-app="app">
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,11 +11,11 @@
 	@else
 		<link rel="stylesheet" type="text/css" href="{{ elixir('css/all.css') }}">
 	@endif
+	<!--
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 
-	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-
+	->
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
@@ -62,7 +62,7 @@
 		</div>
 	</nav>
 
-	@yield('content')
+<div ng-view></div>
 
 	@if(Config::get('app.debug'))
 		<script src="{{ asset('build/js/vendor/jquery.min.js') }}"></script>
@@ -73,6 +73,15 @@
 		<script src="{{ asset('build/js/vendor/angular-messages.min.js') }}"></script>
 		<script src="{{ asset('build/js/vendor/ui-bootstrap.min.js') }}"></script>
 		<script src="{{ asset('build/js/vendor/navbar.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/angular-cookies.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/query-string.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/angular-oauth2.min.js') }}"></script>
+
+
+	<!-- controllers -->
+		<script src="{{ asset('build/js/app.js') }}"></script>
+		<script src="{{ asset('build/js/controllers/login.js') }}"></script>
+		<script src="{{ asset('build/js/controllers/home.js') }}"></script>
 	@else
 		<script src="{{ elixir('js/all.js') }}"></script>
 	@endif
